3c12fcedca8dd5bfa4e375dff15e65fd0f35cd2e
edson angel <edsonangelmc@gmail.com>
2023-04-01 10:18:29 -0600

    first converter release, added buttons, added div for two diferent menus, added some basic style

---
+++ main.js
@@ -0,0 +1,41 @@
+function selTemp() {
+    
+    const mainMenu = document.getElementById("main_menu");
+    const tempMenu = document.getElementById("unit_sel_temp");
+
+    mainMenu.style.display = "none";
+    tempMenu.style.display = "block";
+
+}
+
+
+function temp(option) {
+
+    switch(option) {
+        case 1: 
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+    }
+
+    let val = prompt("Ingrese valor");
+
+    function celtokel() {
+        let cel = prompt("Ingrese valor en grados Celsius: ");
+        let kel = prompt("Ingrese valor en Kelvins");
+        
+    }
+    function celtofah() {
+        let cel = prompt("Ingrese valor en grados Celsius: ");
+        let fah = prompt("Ingrese valor en grados Fahrenheit: ");
+        
+    }
+    function keltofah() {
+        let kel = prompt("Ingrese valor en Kelvins: ");
+        let fah = prompt("Ingrese valor en grados Fahrenheit: ");
+        
+    }
+}
+
